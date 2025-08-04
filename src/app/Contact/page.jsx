af458f6c4c690bf47940bede3395ad4013e5819b
WandererOndra <167094027+WandererOndra@users.noreply.github.com>
2025-08-04 19:57:42 +0200

    Update page.jsx
    update contacts

--- src/app/Contact/page.jsx
+++ src/app/Contact/page.jsx
@@ -23,10 +23,10 @@ export default function Contact() {
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 0 0 2.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 0 1-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 0 0-1.091-.852H4.5A2.25 2.25 0 0 0 2.25 4.5v2.25Z" />
                 </svg>
-                <p>Jana Racková:<a href="tel:+420605904603"> +420 123 456 789</a></p>
+                <p>Jana Racková:<a href="tel:+420605904603"> +420 777 513 536</a></p>
             </div>
             <div className="contactItem md:order-4">
-                <p>IČO 2797495678</p>
+                <p>IČO 1111111111</p>
             </div>
             <div className="contactItem md:order-5">
                 <svg className="w-6 h-6 text-gray-800 dark:text-yellow-700" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
